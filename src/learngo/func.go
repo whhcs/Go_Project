6555e3e38ac5d99f2af487f0fab52d1e7698f35b
AlanHom <66947896@qq.com>
2021-10-27 18:18:17 +0800

    add swap

--- src/learngo/func.go
+++ src/learngo/func.go
@@ -43,6 +43,11 @@ func sum(numbers ...int) int {
 	}
 	return s
 }
+
+func swap(a, b int) (int, int) {
+	return b, a
+}
+
 func main() {
 	if result, err := eval(3, 4, "x"); err != nil {
 		fmt.Println("Error:", err)
@@ -58,4 +63,8 @@ func main() {
 		}, 3, 4))
 
 	fmt.Println(sum(1, 2, 3, 4, 5))
+
+	a, b := 3, 4
+	a, b = swap(a, b)
+	fmt.Println(a, b)
 }
