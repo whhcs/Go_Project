c6e5e4ac2df48697183b7a8f29daa3adda7f8109
AlanHom <66947896@qq.com>
2021-10-30 09:36:36 +0800

    Use composite extension types

--- src/learngo/tree/entry/entry.go
+++ src/learngo/tree/entry/entry.go
@@ -1,6 +1,26 @@
 package main
 
-import "github.com/whhcs/learngo/tree"
+import (
+	"fmt"
+
+	"github.com/whhcs/learngo/tree"
+)
+
+type myTreeNode struct {
+	node *tree.Node
+}
+
+func (myNode *myTreeNode) postOrder() {
+	if myNode == nil || myNode.node == nil {
+		return
+	}
+	left := myTreeNode{myNode.node.Left}
+	left.postOrder()
+
+	right := myTreeNode{myNode.node.Right}
+	right.postOrder()
+	myNode.node.Print()
+}
 
 func main() {
 	var root tree.Node
@@ -19,4 +39,9 @@ func main() {
 	root.Right.Left.SetValue(4)
 
 	root.Traverse()
+	fmt.Println()
+
+	MyRoot := myTreeNode{&root}
+	MyRoot.postOrder()
+	fmt.Println()
 }
