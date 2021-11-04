feda1053b8a84ca9e930b2de66b1d49e122b615e
AlanHom <66947896@qq.com>
2021-11-04 09:20:00 +0800

    interface 定义、实现、值类型、组合

---
+++ src/learngo/retriever/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/whhcs/learngo/retriever/mock"
+	"github.com/whhcs/learngo/retriever/real"
+)
+
+type Retriever interface {
+	Get(url string) string
+}
+
+type Poster interface {
+	Post(url string,
+		form map[string]string) string
+}
+
+const url = "https://www.imooc.com"
+
+func download(r Retriever) string {
+	return r.Get(url)
+}
+
+func post(poster Poster) {
+	poster.Post(url,
+		map[string]string{
+			"name":   "ccmouse",
+			"course": "golang",
+		})
+}
+
+type RetrieverPoster interface {
+	Retriever
+	Poster
+}
+
+func seesion(s RetrieverPoster) string {
+	s.Post(url, map[string]string{
+		"contents": "another fake imooc.com",
+	})
+	return s.Get(url)
+}
+
+func inspect(r Retriever) {
+	fmt.Printf("%T %v\n", r, r)
+	fmt.Println("Type switch:")
+	switch v := r.(type) {
+	case *mock.Retriever:
+		fmt.Println("Contents:", v.Contents)
+	case *real.Retriever:
+		fmt.Println("UserAgent:", v.UserAgent)
+	}
+}
+
+func main() {
+	var r Retriever
+
+	retriever := mock.Retriever{
+		"this is fake imooc.com"}
+	r = &retriever
+	inspect(r)
+
+	r = &real.Retriever{
+		UserAgent: "Mozilla/5.0",
+		TimeOut:   time.Minute,
+	}
+	inspect(r)
+
+	// Type assertion, 判断值类型
+	if mockRetriever, ok := r.(*mock.Retriever); ok {
+		fmt.Println(mockRetriever.Contents)
+	} else {
+		fmt.Println("not a mock retriever")
+	}
+
+	fmt.Println("Try a session")
+	fmt.Println(seesion(&retriever))
+}
