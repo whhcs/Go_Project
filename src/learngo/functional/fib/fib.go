b8fda6d62fc0c91d48be9b22471c85102fca1f4f
AlanHom <66947896@qq.com>
2021-11-12 18:22:16 +0800

    使用defer调用及错误处理

--- src/learngo/functional/fib/fib.go
+++ src/learngo/functional/fib/fib.go
@@ -1,41 +1,10 @@
-package main
+package fib
 
-import (
-	"bufio"
-	"fmt"
-	"io"
-	"strings"
-)
-
-func fibonacci() intGen {
+// 1 1 2 3 5 8 13 21 ...
+func Fibonacci() func() int {
 	a, b := 0, 1
 	return func() int {
 		a, b = b, a+b
 		return a
 	}
-}
-
-type intGen func() int
-
-func (g intGen) Read(p []byte) (n int, err error) {
-	next := g()
-	if next > 10000 {
-		return 0, io.EOF
-	}
-	s := fmt.Sprintf("%d\n", next)
-	// TODO : incorrect if p is too small!
-	return strings.NewReader(s).Read(p)
-}
-
-func printFileContents(reader io.Reader) {
-	scanner := bufio.NewScanner(reader)
-
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-	}
-}
-
-func main() {
-	f := fibonacci()
-	printFileContents(f)
 }
