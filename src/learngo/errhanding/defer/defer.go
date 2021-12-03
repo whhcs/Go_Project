86a2cfadf93e4b107a5ebfe499fa07ec7be7f77d
AlanHom <66947896@qq.com>
2021-12-03 15:40:45 +0800

    使用defer调用及错误处理

--- src/learngo/errhanding/defer/defer.go
+++ src/learngo/errhanding/defer/defer.go
@@ -18,17 +18,16 @@ func tryDefer() {
 	}
 }
 
-// 1 1 2 3 5 8 13 21 ...
 func writeFile(filename string) {
-	file, err := os.OpenFile(filename, os.O_EXCL|os.O_CREATE, 0666)
+	file, err := os.OpenFile(filename,
+		os.O_EXCL|os.O_CREATE, 0666)
 
 	err = errors.New("this is a custom error")
 	if err != nil {
 		if pathError, ok := err.(*os.PathError); !ok {
 			panic(err)
 		} else {
-			fmt.Printf("%s, %s, %s\n",
-				pathError.Op,
+			fmt.Printf("%s, %s, %s\n", pathError.Op,
 				pathError.Path,
 				pathError.Err)
 		}
