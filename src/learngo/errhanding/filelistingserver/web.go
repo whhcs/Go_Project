11c0dabd9203d3086bb7b17861add77e16cfe216
AlanHom <66947896@qq.com>
2021-12-12 19:21:31 +0800

    panic和recover

--- src/learngo/errhanding/filelistingserver/web.go
+++ src/learngo/errhanding/filelistingserver/web.go
@@ -16,11 +16,29 @@ func errWrapper(
 	http.ResponseWriter, *http.Request) {
 	return func(writer http.ResponseWriter,
 		request *http.Request) {
+		defer func() {
+			if r := recover(); r != nil {
+				log.Printf("Panic: %v", r)
+				http.Error(writer,
+					http.StatusText(http.StatusInternalServerError),
+					http.StatusInternalServerError)
+			}
+		}()
+
 		err := handler(writer, request)
+
 		if err != nil {
 			log.Printf("Error occurred "+
 				"handling request: %s",
 				err.Error())
+
+			if userErr, ok := err.(userError); ok {
+				http.Error(writer,
+					userErr.Message(),
+					http.StatusBadRequest)
+				return
+			}
+
 			code := http.StatusOK
 			switch {
 			case os.IsNotExist(err):
@@ -36,9 +54,14 @@ func errWrapper(
 	}
 }
 
+type userError interface {
+	error
+	Message() string
+}
+
 func main() {
 	// url 里输入一个文件的地址，显示文件的内容
-	http.HandleFunc("/list/", errWrapper(filelisting.HandleFileList))
+	http.HandleFunc("/", errWrapper(filelisting.HandleFileList))
 
 	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
