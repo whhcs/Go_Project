76d01c2103294faf61d7692bfb5643915751c3bf
AlanHom <66947896@qq.com>
2021-12-12 15:49:34 +0800

    使用defer调用以及实现服务器统一的出错处理逻辑（1）

--- src/learngo/errhanding/defer/defer.go
+++ src/learngo/errhanding/defer/defer.go
@@ -19,15 +19,16 @@ func tryDefer() {
 }
 
 func writeFile(filename string) {
-	file, err := os.OpenFile(filename,
-		os.O_EXCL|os.O_CREATE, 0666)
+	file, err := os.OpenFile(
+		filename, os.O_EXCL|os.O_CREATE, 0666)
 
 	err = errors.New("this is a custom error")
 	if err != nil {
 		if pathError, ok := err.(*os.PathError); !ok {
 			panic(err)
 		} else {
-			fmt.Printf("%s, %s, %s\n", pathError.Op,
+			fmt.Printf("%s, %s, %s\n",
+				pathError.Op,
 				pathError.Path,
 				pathError.Err)
 		}
