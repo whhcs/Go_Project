0c5c4da1da2daff57ca8647c317b9b378da701ba
AlanHom <66947896@qq.com>
2021-12-12 19:22:51 +0800

    panic和recover

--- src/learngo/errhanding/filelistingserver/filelisting/handler.go
+++ src/learngo/errhanding/filelistingserver/filelisting/handler.go
@@ -4,11 +4,28 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 )
+
+const prefix = "/list/"
+
+type userError string
+
+func (e userError) Error() string {
+	return e.Message()
+}
+
+func (e userError) Message() string {
+	return string(e)
+}
 
 func HandleFileList(writer http.ResponseWriter,
 	request *http.Request) error {
-	path := request.URL.Path[len("/list/"):] // /list/fib.txt
+	if strings.Index(request.URL.Path, prefix) != 0 {
+		return userError("path must start " +
+			"with " + prefix)
+	}
+	path := request.URL.Path[len(prefix):] // /list/fib.txt
 	file, err := os.Open(path)
 	if err != nil {
 		return err
